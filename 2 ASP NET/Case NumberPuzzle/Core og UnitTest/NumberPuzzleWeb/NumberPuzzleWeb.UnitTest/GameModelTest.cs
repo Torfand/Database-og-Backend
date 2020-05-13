e7faa97adeb7d9e3daf422d69825ba1c1ab0894a
Torfand <Torfin@getacademy.no>
2020-05-13 14:44:11 +0200

    x

--- 2 ASP NET/Case NumberPuzzle/Core og UnitTest/NumberPuzzleWeb/NumberPuzzleWeb.UnitTest/GameModelTest.cs
+++ 2 ASP NET/Case NumberPuzzle/Core og UnitTest/NumberPuzzleWeb/NumberPuzzleWeb.UnitTest/GameModelTest.cs
@@ -1,3 +1,4 @@
+using NumberPuzzleWeb.Core.Domain_Model;
 using NUnit.Framework;
 
 namespace NumberPuzzleWeb.UnitTest
@@ -12,13 +13,13 @@ namespace NumberPuzzleWeb.UnitTest
         public void TestIsNotSolved()
         {
             var gameModel = new GameModel(new[] { 1, 2, 3, 4, 5, 6, 7, 0, 8 });
-            Assert.AreEqual(false, gameModel.IsSolved):
+            Assert.AreEqual(false, gameModel.IsSolved);
         }
         [Test]
-        public void TestIsNotSolved()
+        public void TestIsSolved()
         {
             var gameModel = new GameModel(new[] { 1, 2, 3, 4, 5, 6, 7, 8, 0 });
-            Assert.AreEqual(false, gameModel.IsSolved):
+            Assert.AreEqual(true, gameModel.IsSolved);
         }
     }
 }
---
+++ 2 ASP NET/Case NumberPuzzle/Kjørbar Appliikasjon med in memory database/NumberPuzzleWeb/NumberPuzzleWeb/Controllers/GameController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NumberPuzzleWeb.API.ViewModel;
+using NumberPuzzleWeb.Core.ApplicationServices;
+using NumberPuzzleWeb.Core.Domain_Model;
+
+namespace NumberPuzzleWeb.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        private GameService _gameService;
+        public GameController(GameService gameService)
+        {
+            _gameService = gameService;
+        }
+        [HttpGet]
+        public async Task<GameViewModel> StartGame()
+        {
+            var game = await _gameService.StartGame();
+              return ViewFromDomainModel(game);
+        }
+
+
+        [HttpGet("{gameID})")]
+        public async Task<GameViewModel> Read(Guid gameID)
+        {
+            var game = await _gameService.Read(gameID);
+            return   ViewFromDomainModel(game);
+        }
+        [HttpPost]
+        public async Task<GameViewModel> Play(int index, Guid gameID)
+        {
+            var game = await _gameService.Play(index, gameID);
+            return ViewFromDomainModel(game);
+        }
+        private static GameViewModel ViewFromDomainModel(GameModel game)
+        {
+            return new GameViewModel(game.Id.ToString(), game.PlayCount,game.IsSolved, game.Numbers );
+        }
+    }
+}
