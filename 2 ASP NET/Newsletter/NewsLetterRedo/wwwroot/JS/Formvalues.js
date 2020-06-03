0fdf547753ae2e62bf758beef88c89645d19253f
Torfand <Torfin@getacademy.no>
2020-06-03 07:43:39 +0200

    x

--- 2 ASP NET/Newsletter/NewsLetterRedo/wwwroot/JS/Formvalues.js
+++
@@ -1,8 +0,0 @@
-function getValues() {
-
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    if (!name || !email)return alert("Fields need values");
-        
-     subscribe(name, email);
-}
